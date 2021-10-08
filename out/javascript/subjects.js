b19300677b119e96527df5a8c9b3dd76f43024e1
Wroppy <wongw@scotscollege.school.nz>
2021-10-08 22:56:39 +1300

    Finished subjects page

--- out/javascript/subjects.js
+++ out/javascript/subjects.js
@@ -10,11 +10,13 @@ function onClick(i){
     if (elementClasses.includes("show")){
         // Changes the class name in order to hide the drop down and the icons
         dropdowns[i].className = elementClasses.replace("show", "hide")
-        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_down_black", "pointer_up_black")
+        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_up_black", "pointer_down_black")
+
     }else{
         // Changes the class name in order to hide the drop down and the icons
         dropdowns[i].className = elementClasses.replace("hide", "show")
-        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_up_black", "pointer_down_black")
+        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_down_black", "pointer_up_black")
+
     }
 }
 
