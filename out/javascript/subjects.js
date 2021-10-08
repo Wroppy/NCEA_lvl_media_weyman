afea49c7371ef1603f5cbd4c2b22ea2e31e519c0
Wroppy <wongw@scotscollege.school.nz>
2021-10-08 22:02:45 +1300

    Completed version 1 of the subjects page

---
+++ out/javascript/subjects.js
@@ -0,0 +1,24 @@
+let dropDownButtons = document.getElementsByClassName("nt-subject__dropdown-button");
+let dropdowns = document.getElementsByClassName("nt-subject__dropdown")
+
+function onClick(i){
+    let elementClasses = dropdowns[i].className
+    elementClasses = elementClasses.toString()
+    let dropdownButtonClasses = dropDownButtons[i].className
+
+    // Checks whether it is hidden or not
+    if (elementClasses.includes("show")){
+        // Changes the class name in order to hide the drop down and the icons
+        dropdowns[i].className = elementClasses.replace("show", "hide")
+        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_down_black", "pointer_up_black")
+    }else{
+        // Changes the class name in order to hide the drop down and the icons
+        dropdowns[i].className = elementClasses.replace("hide", "show")
+        dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_up_black", "pointer_down_black")
+    }
+}
+
+for (let i=0; i<dropDownButtons.length; i++){
+    dropDownButtons[i].onclick = e => onClick(i)
+
+}
