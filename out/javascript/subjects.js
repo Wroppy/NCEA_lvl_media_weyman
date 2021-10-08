3187e41215b28f91dc989414652df70c1ba9afd6
Wroppy <wongw@scotscollege.school.nz>
2021-10-08 23:15:02 +1300

    Added functionality to the whole subject box

--- out/javascript/subjects.js
+++ out/javascript/subjects.js
@@ -1,26 +1,30 @@
-let dropDownButtons = document.getElementsByClassName("nt-subject__dropdown-button");
+let dropDownBox = document.getElementsByClassName("nt-content__subject-boxes");
 let dropdowns = document.getElementsByClassName("nt-subject__dropdown")
+let dropDownButtons = document.getElementsByClassName("nt-subject__dropdown-button")
 
-function onClick(i){
+function onClick(i) {
     let elementClasses = dropdowns[i].className
-    elementClasses = elementClasses.toString()
     let dropdownButtonClasses = dropDownButtons[i].className
+    elementClasses = elementClasses.toString()
+
 
     // Checks whether it is hidden or not
-    if (elementClasses.includes("show")){
+    if (elementClasses.includes("show")) {
         // Changes the class name in order to hide the drop down and the icons
         dropdowns[i].className = elementClasses.replace("show", "hide")
         dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_up_black", "pointer_down_black")
-
-    }else{
+    } else {
+        console.log("do")
         // Changes the class name in order to hide the drop down and the icons
         dropdowns[i].className = elementClasses.replace("hide", "show")
+
         dropDownButtons[i].className = dropdownButtonClasses.replace("pointer_down_black", "pointer_up_black")
+
 
     }
 }
 
-for (let i=0; i<dropDownButtons.length; i++){
-    dropDownButtons[i].onclick = e => onClick(i)
+for (let i = 0; i < dropDownBox.length; i++) {
+    dropDownBox[i].onclick = e => onClick(i)
 
 }
