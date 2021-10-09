4e6d748bc0f1eec24749bd049dd7b91635d40f7c
Wroppy <wongw@scotscollege.school.nz>
2021-10-09 23:31:34 +1300

    Successfully change questions on the page

--- out/javascript/quiz_template.js
+++ out/javascript/quiz_template.js
@@ -8,14 +8,68 @@ async function name() {
 }
 
 async function main() {
+    function changeActiveElement(num) {
+        try {
+            document.getElementById("active").id = "";
+        } catch {
+        }
 
-    let questions = await name();
-
-    console.log(questions)
-    for (let question of questions) {
-        console.log(question);
+        // Selects the appropriate element in the side bar and sets it to active id
+        document.getElementsByClassName("nt-quiz__sidebar-tab")[num].id = "active";
 
     }
+
+
+    function changeQuestionText(num) {
+        // Sets the question text top the question
+        document.getElementById("nt-question-text").textContent = questions[num].question;
+
+    }
+
+    function changeAnswerTexts(num){
+        // Sets the answer elements to the right text
+        for (let i = 0; i < answerLabels.length; i++) {
+            answerLabels[i].textContent = questions[num].question_choice[i];
+
+        }
+    }
+
+    // Changes the question given the number
+    function changeQuestion(num){
+        changeActiveElement(0);
+
+        changeQuestionText(0);
+
+        changeAnswerTexts(0)
+    }
+
+
+    let questions = await name();
+    let answerElements = document.getElementsByClassName("nt-quiz__answer");
+    let answerLabels = document.getElementsByClassName("nt-quiz__answer-label")
+
+
+    console.log(questions)
+
+    for (let i=0; i<questions.length; i++) {
+        // Creates the necessary side tabs
+        const e = document.createElement("div");
+        e.innerHTML = "Question " + questions[i].question_number;
+        e.className = "nt-quiz__sidebar-tab";
+        document.getElementById("nt-quiz__sidebar").appendChild(e);
+
+        // Sets the onclick event to change the question
+
+
+        console.log(questions[i]);
+    }
+
+    // Sets it to question 0
+    changeQuestion(0);
+
+
+
+
 }
 
 main()
