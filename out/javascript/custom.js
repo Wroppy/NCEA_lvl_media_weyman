264d957e3cb746ec69f9f5616b604773416e51fc
Wroppy <wongw@scotscollege.school.nz>
2021-11-05 23:44:32 +1300

    Added ability to add custom tabs using JS

--- out/javascript/custom.js
+++ out/javascript/custom.js
@@ -1,21 +1,57 @@
 // Redirects the user to the quiz they made
 function redirectUsers(url) {
-    // Splits it into its components slices everything except the last /, then joins it back together
-    let urlStart = window.location.href.split("/").slice(0, -1).join("/");
+    let urlStart = window.location.origin;
 
     // Redirects the user's url
-    window.location.href = urlStart + "/" + url;
+    window.location.href = urlStart + "/custom/" + url;
 }
 
-function addQuizTab(quiz) {
-    let author = quiz.author;
-    let quizName = quiz.quizName;
-    let quizDescription = quiz.description;
-    let questionNumber = quiz.questions.length;
-    let url = quiz.url;
+function addQuizTab(quizInfo) {
+    let author = quizInfo.author;
+    let quizName = quizInfo.quizName;
+    let quizDescription = quizInfo.description;
+    let questionNumber = quizInfo.questionLength;
+    let url = quizInfo.url;
 
 
-    let e = 1;
+    // Creates the quiz tab element and appends it to the window
+    let quizTab = document.createElement("div");
+    quizTab.className = "nt-custom__user-quiz-tab";
+
+    let authorElement = document.createElement("div");
+    authorElement.className = "nt-user-quiz-tab__author nt-user-quiz-tab__content";
+    let authorSpan = document.createElement("span");
+    authorSpan.innerHTML = author;
+    authorElement.appendChild(authorSpan);
+
+    let quizNameElement = document.createElement("div");
+    quizNameElement.className = "nt-user-quiz-tab__quiz-name nt-user-quiz-tab__content";
+    let quizNameSpan = document.createElement("span");
+    quizNameSpan.innerHTML = quizName;
+    quizNameElement.appendChild(quizNameSpan);
+
+    let descriptionElement = document.createElement("div");
+    descriptionElement.className = "nt-user-quiz-tab__preview-headers nt-user-quiz-tab__content";
+    let descriptionSpan = document.createElement("span");
+    descriptionSpan.innerHTML = quizDescription;
+    descriptionElement.appendChild(descriptionSpan);
+
+    let quizLengthElement = document.createElement("div");
+    quizLengthElement.className = "nt-user-quiz-tab__question-num nt-user-quiz-tab__content";
+    let quizLengthSpan = document.createElement("span");
+    quizLengthSpan.innerHTML = questionNumber.toString() + "Questions";
+    quizLengthElement.appendChild(quizLengthSpan);
+
+    quizTab.appendChild(authorElement);
+    quizTab.appendChild(quizNameElement);
+    quizTab.appendChild(descriptionElement);
+    quizTab.appendChild(quizLengthElement);
+
+    quizTab.onclick = () => redirectUsers(url);
+
+    document.querySelector(".nt-custom__user-quiz-wrapper").appendChild(quizTab);
+
+
 }
 
 
@@ -24,10 +60,7 @@ function main() {
         author: "Weyman Wong",
         quizName: "Testing",
         description: "description test",
-        questions: [
-            {},
-            {}
-        ],
+        questionLength: 4,
         url: "1"
     }
 
